5b4eebba601c6cc76ce64d0e87a46fd77743cd4d
suryateja357 <tekisuruyateja@gmail.com>
2026-05-25 15:28:28 +0000

    ec2 with sg attached

--- main.tf
+++ main.tf
@@ -1,15 +1,42 @@
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = var.my_bucket
+resource "aws_instance" "first_ec2" {
+  instance_type   = var.instance_type
+  ami             = "ami-05ffe3c48a9991133"
+  security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
+    Name        = var.instance_name
+    environment = var.environment
     team        = var.team_name
-    Environment = var.environment
+    region      = var.region
+  }
+}
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  tags        = {
+    environment = var.environment
+    team        = var.team_name
+    region      = var.region
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_s3_bucket_versioning" "my-bucket-versioning" {
-  bucket = aws_s3_bucket.my-bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+
--- variables.tf
+++ variables.tf
@@ -1,26 +1,15 @@
-variable "my_bucket" {
+variable "instance_type" {
   type = string
-
-  validation {
-    condition     = length(var.my_bucket) > 0
-    error_message = "The bucket name must not be empty."
-  }
 }
-
-variable "team_name" {
+variable "instance_name" {
   type = string
-
-  validation {
-    condition     = length(var.team_name) > 0
-    error_message = "The team name must not be empty."
-  }
 }
-
+variable "region" {
+  type = string
+}
 variable "environment" {
   type = string
-
-  validation {
-    condition     = length(var.environment) > 0
-    error_message = "The environment name must not be empty."
-  }
+}
+variable "team_name" {
+  type = string
 }
